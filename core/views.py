fe253327607e5aa5ba1a017d5373b4ba2126e8b2
Theborycuas <borys_jair@hotmail.com>
2020-02-14 10:06:22 -0500

    Subida Blog y Chef COmpleta2

--- core/views.py
+++ core/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
 from menu.models import Menu
+from blog.models import Post
 # Create your views here.
 def home(request):
     menus = Menu.objects.all()
-    return render(request, "core/home.html", {'menus':menus})
+    posts = Post.objects.all()
+    return render(request, "core/home.html", {'menus':menus, 'posts':posts})
 
 def about(request):
     return render(request, "core/about.html")
