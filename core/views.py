876f5c7086c392e35de45abbc001d25a1b8603bb
Theborycuas <borys_jair@hotmail.com>
2020-02-14 02:47:00 -0500

    Subida3311imagen1

--- core/views.py
+++ core/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
-
+from menu.models import Menu
 # Create your views here.
 def home(request):
-    return render(request, "core/home.html")
+    menus = Menu.objects.all()
+    return render(request, "core/home.html", {'menus':menus})
 
 def about(request):
     return render(request, "core/about.html")
